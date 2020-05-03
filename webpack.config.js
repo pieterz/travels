512a1a28e28f72f5eda864e022c2b9ab92c30411
Pieter <pieterzitman@hotmail.com>
2020-05-03 02:05:33 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
 module.exports = {
    entry: './app/assets/scripts/App.js',
    output: {
@@ -7,5 +14,13 @@ module.exports = {
        path: path.resolve(__dirname, 'app')
    },
    mode: 'development',
-   watch: true
+   watch: true,
+   module: {
+       rules: [
+           {
+               test: /\.css$/i,
+               use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+           }
+       ]
+   }
 }
